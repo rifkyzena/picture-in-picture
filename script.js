e048e25ac7ec6d55b361533a13f2a4b877ca213f
frazeinky <rifkyzena26@gmail.com>
2022-08-31 23:44:53 +0700

    hello world

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log('testing');
 // perkuat devtools, bakal sering buka buka terus
 // inspect element check network, check console, bisa liat macem macem
+
+// new terminal caranya git init, git add ., git commit + message, git push origin master
