efb6b4607771e9258c62529ed2b66f1e4d0427c3
frazeinky <rifkyzena26@gmail.com>
2022-09-10 21:29:35 +0700

    FINALIZED

--- script.js
+++ script.js
@@ -4,8 +4,28 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream(){
     try {
-        
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
     } catch (error) {
-        
+        // Catch ERROR HERE
+        console.log('whoops, error here:', error);
     }
 }
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+
+    //Reset Button
+    button.disabled = false;
+
+});
+
+// On Load
+selectMediaStream();
